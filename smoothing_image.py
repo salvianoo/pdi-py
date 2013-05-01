9bf78c6c7cb017eb70cdbbbf9372938df98438bd
Salviano Ludgerio <salviano.lfg@gmail.com>
2013-05-01 08:55:15 -0300

    Adds parentheses for line continuation
    Refactoring: A good refactoring is use parentheses
    instead of backslash for readability.

--- smoothing_image.py
+++ smoothing_image.py
@@ -10,21 +10,20 @@ for i in xrange(1, width - 1):
         medias = list()
 
         for color in xrange(3):
-            sum_of_neighbors = \
-                    data[i - 1, j][color] + \
-                    data[i + 1, j][color] + \
-                    data[i, j - 1][color] + \
-                    data[i, j + 1][color] + \
-                    data[i - 1, j - 1][color] + \
-                    data[j + 1, j - 1][color] + \
-                    data[i + 1, j + 1][color] + \
-                    data[i - 1, j + 1][color] + \
-                    data[i, j][color]
-
+            sum_of_neighbors = (
+                data[i - 1, j][color] +
+                data[i + 1, j][color] +
+                data[i, j - 1][color] +
+                data[i, j + 1][color] +
+                data[i - 1, j - 1][color] +
+                data[j + 1, j - 1][color] +
+                data[i + 1, j + 1][color] +
+                data[i - 1, j + 1][color] +
+                data[i, j][color]
+            )
             medias.append(sum_of_neighbors / 9)
 
         data[i, j] = medias[0], medias[1], medias[2]
-        # media_pixel = media_r / 9, media_b / 9, media_g / 9
 
 image.show()
 # image.save("output_image.jpg", "JPEG")
